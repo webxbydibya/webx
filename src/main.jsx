4d7515247c4d04de4f90f174789018a7f1345d3b
DibyaJyoti Parida <dibyajyotiparida940@gmail.com>
2025-04-29 21:24:41 +0530

    adding vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Page from './page'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Page />
+    <Analytics />
   </StrictMode>,
 )
