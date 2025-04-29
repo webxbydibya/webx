6b9bca7f71861eb7b08c0861ff5ae5d6b2249c25
DibyaJyoti Parida <dibyajyotiparida940@gmail.com>
2025-04-29 18:59:35 +0530

    2nd commit

--- src/page.jsx
+++ src/page.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Header from "./components/header"; 
 import Footer from "./components/footer";  
-
 import Page1 from "./page1.jsx"
 import Page2 from "./page2.jsx";
 import Page3 from "./page3.jsx"
@@ -10,23 +8,22 @@ import Privacy from './privacy.jsx';
 import TermsAndConditions from './terms.jsx';
 import ContactUs from './contact.jsx';
 import Error from './error.jsx';
-export default function Page(){
-    return(
-        <>
+
+export default function Page() {
+    return (
+        <Router>
             <Header />
-            <Router>
-                <Routes>
-                    <Route path="/:id" element={<Page1 />} />
-                    <Route path="/:id/2" element={<Page2 />} />
-                    <Route path="/:id/3" element={<Page3 />} />
-                    <Route path="/privacy" element={<Privacy />} />
-                    <Route path="/terms" element={<TermsAndConditions />} />
-                    <Route path="/contact" element={<ContactUs />} />
-                    <Route path='*' element={<Error />} />
-                </Routes>
-            </Router>
-           <hr />
-           <Footer />
-        </>
-    )
+            <Routes>
+                <Route path="/:id" element={<Page1 />} />
+                <Route path="/:id/2" element={<Page2 />} />
+                <Route path="/:id/3" element={<Page3 />} />
+                <Route path="/privacy" element={<Privacy />} />
+                <Route path="/terms" element={<TermsAndConditions />} />
+                <Route path="/contact" element={<ContactUs />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            <hr />
+            <Footer />
+        </Router>
+    );
 }
